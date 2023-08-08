86e2e53c6787a97330008971ae996577425984a8
redCat <yana.sitkowecz@yandex.ru>
2023-08-08 11:37:57 +0300

    Add test check_state

--- PAC/common/PAC_dev.h
+++ PAC/common/PAC_dev.h
@@ -4951,7 +4951,7 @@ class device_manager: public i_Lua_save_device
         char is_first_device[ device::C_DEVICE_TYPE_CNT ] = { 0 };
 
         dev_stub stub;  ///< Устройство-заглушка, фиктивное устройство.
-        unsigned int active_counter;
+        int active_counter; //< Количество включенных устройств
         struct range    ///< Диапазон устройств одного типа.
             {
             int start_pos;
@@ -4971,7 +4971,7 @@ class device_manager: public i_Lua_save_device
         int get_device_n( const char* dev_name );
 
         std::vector< device* > project_devices; ///< Все устройства.
-        std::vector<bool> previous_states;
+        std::vector<bool> previous_states; //< Предыдущие состояния устройств 
 
         /// @brief Единственный экземпляр класса.
         static auto_smart_ptr < device_manager > instance;
@@ -4987,7 +4987,11 @@ class device_manager: public i_Lua_save_device
         //
         // Для тестирования.
         void clear_io_devices();
-        void check_state();
+
+        // @brief Подсчет включенных устройств.
+        //
+        // Проверяет изменилось ли состояние.
+        int check_state();
     };
 //-----------------------------------------------------------------------------
 /// @brief таймер.
